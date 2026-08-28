42c1920d01c713669f3a6b4177e372fb61fb9abc
notmeegoreng <notmeegoreng@gmail.com>
2026-08-28 13:23:51 +0800

    add list functionality

--- src/main/java/Clue.java
+++ src/main/java/Clue.java
@@ -8,7 +8,7 @@ public class Clue {
                 + "| |    | |    | |  | | |___ \n"
                 + "| |    | |    | |  | |  ___| \n"
                 + "| |____| |____| |__| | |___ \n"
-                + " \\_____|______|\\____/|_____| \n";
+                + " \\_____|______|\\____/|_____| \n";s
         String separator = "____________________________________________________________";
 
         System.out.println(separator);
@@ -18,6 +18,9 @@ public class Clue {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             System.out.println(separator);
@@ -28,7 +31,16 @@ public class Clue {
                 break;
             }
 
-            System.out.println(" " + command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else if (taskCount < tasks.length) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
+            }
+
             System.out.println(separator);
         }
     }
