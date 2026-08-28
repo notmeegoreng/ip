c8fcea3141b236c2c67c7b5e1a579757d035106f
notmeegoreng <notmeegoreng@gmail.com>
2026-08-28 13:17:56 +0800

    Readd Clue.java, and greet the user

---
+++ src/main/java/Clue.java
@@ -0,0 +1,20 @@
+public class Clue {
+    public static void main(String[] args) {
+        String banner =
+                "  _____ _      _    _ _____ \n"
+                + " / ____| |    | |  | |  ___|\n"
+                + "| |    | |    | |  | | |___ \n"
+                + "| |    | |    | |  | |  ___| \n"
+                + "| |____| |____| |__| | |___ \n"
+                + " \\_____|______|\\____/|_____| \n";
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
+        System.out.println(banner);
+        System.out.println("Hello! I'm Clue.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
+    }
+}
