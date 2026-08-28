958ebae32733bca9989cdb19e1dfe85c29aadbc9
notmeegoreng <notmeegoreng@gmail.com>
2026-08-28 13:21:07 +0800

    add echo functionality

--- src/main/java/Clue.java
+++ src/main/java/Clue.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Clue {
     public static void main(String[] args) {
         String banner =
@@ -14,7 +16,20 @@ public class Clue {
         System.out.println("Hello! I'm Clue.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(" " + command);
+            System.out.println(separator);
+        }
     }
 }
