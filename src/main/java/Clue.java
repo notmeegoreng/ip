78e5dabc14167092475882ff2e2878c5992504ae
notmeegoreng <notmeegoreng@gmail.com>
2026-08-28 15:55:24 +0800

    Add delete task functionality

--- src/main/java/Clue.java
+++ src/main/java/Clue.java
@@ -3,21 +3,21 @@ import tasks.Event;
 import tasks.Task;
 import tasks.ToDo;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Clue {
-    private static int taskCount;
-    private static Task[] tasks;
+    private static ArrayList<Task> tasks;
 
     public static void main(String[] args) {
-        String banner =
-                "  _____ _      _    _ _____ \n"
-                + " / ____| |    | |  | |  ___|\n"
-                + "| |    | |    | |  | | |___ \n"
-                + "| |    | |    | |  | |  ___| \n"
-                + "| |____| |____| |__| | |___ \n"
-                + " \\_____|______|\\____/|_____| \n";
+        String banner = """
+                          _____ _      _    _ _____\s
+                         / ____| |    | |  | |  ___|
+                        | |    | |    | |  | | |___\s
+                        | |    | |    | |  | |  ___|\s
+                        | |____| |____| |__| | |___\s
+                         \\_____|______|\\____/|_____|\s
+                        """;
         String separator = "____________________________________________________________";
 
         System.out.println(separator);
@@ -27,8 +27,7 @@ public class Clue {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
-        tasks = new Task[100];
-        taskCount = 0;
+        tasks = new ArrayList<Task>();
 
         while (scanner != null && scanner.hasNextLine()) {
             String in = scanner.nextLine();
@@ -41,8 +40,11 @@ public class Clue {
                     scanner = null;
                 }
                 case "list" -> {
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    if (tasks.isEmpty())  {
+                        System.out.println("nothing here...");
+                    }
+                    for (int i = 1; i <= tasks.size(); i++) {
+                        System.out.println(" " + i + ". " + tasks.get(i - 1));
                     }
                 }
                 case "mark" -> {
@@ -50,8 +52,9 @@ public class Clue {
                         System.out.println("Please tell us which task to mark!");
                         break;
                     }
-                    Task t = getTaskFromInput(input[1]);
-                    if (t != null) {
+                    int idx = getIndexFromInput(input[1]);
+                    if (idx != -1) {
+                        Task t = tasks.get(idx);
                         t.setDone(true);
                         System.out.println("Woohoo! I've marked this task as done:");
                         System.out.print("\t");
@@ -63,13 +66,24 @@ public class Clue {
                         System.out.println("Please tell us which task to unmark!");
                         break;
                     }
-                    Task t = getTaskFromInput(input[1]);
-                    if (t != null) {
+                    int idx = getIndexFromInput(input[1]);
+                    if (idx != -1) {
+                        Task t = tasks.get(idx);
                         t.setDone(false);
                         System.out.println("OK, I've marked this task as unfinished:");
                         System.out.print("\t");
                         System.out.println(t);
                     }
+                }
+                case "delete" -> {
+                    if (input.length == 1) {
+                        System.out.println("Please tell us which task to delete!");
+                        break;
+                    }
+                    Task t = tasks.remove(getIndexFromInput(input[1]));
+                    System.out.print("Alright, deleted this task:\n\t");
+                    System.out.println(t);
+                    reportCount();
                 }
                 case "todo" -> {
                     if (input.length == 1) {
@@ -102,29 +116,36 @@ public class Clue {
         }
     }
 
-    static Task getTaskFromInput(String unparsedInt) {
+    static int getIndexFromInput(String unparsedInt) {
         int idx;
         try {
             idx = Integer.parseInt(unparsedInt) - 1;
         } catch (NumberFormatException e) {
             System.out.println("Invalid task number! Please provide a positive integer!");
-            return null;
+            return -1;
         }
         if (idx < 0) {
             System.out.println("Invalid task number! Please provide a positive integer!");
-        } else if (idx >= taskCount) {
+        } else if (idx >= tasks.size()) {
             System.out.println("Invalid task number! Not enough recorded tasks!");
         } else {
-            return tasks[idx];
+            return idx;
         }
-        return null;
+        return -1;
     }
 
     static void addTask(Task task) {
-        tasks[taskCount] = task;
-        taskCount++;
-        System.out.print("Noted. I've added this task:\n\t%");
+        tasks.add(task);
+        System.out.print("Noted. I've added this task:\n\t");
         System.out.println(task);
-        System.out.printf("Now, there are %d task%s in the list.\n", taskCount, taskCount == 1 ? "" : "s");
+        reportCount();
+    }
+
+    static void reportCount() {
+        System.out.printf(
+                "Now, there %s %d task%s in the list.\n",
+                tasks.size() == 1 ? "is": "are",
+                tasks.size(),
+                tasks.size() == 1 ? "" : "s");
     }
 }
