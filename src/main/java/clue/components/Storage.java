381cd3c940d1f599ad215b263fb26c04365df22e
notmeegoreng <notmeegoreng@gmail.com>
2026-08-31 03:17:28 +0800

    Add JavaDoc comments for Storage and TaskList

--- src/main/java/clue/components/Storage.java
+++ src/main/java/clue/components/Storage.java
@@ -60,6 +60,12 @@ public class Storage {
         return lst;
     }
 
+    /**
+     * Persists the tasks to the file, for later retrieval with {@link load}
+     *
+     * @param tasks - The list of tasks to save.
+     * @return - Whether this operation is successful.
+     */
     public boolean save(List<Task> tasks) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE))) {
             for (Task t : tasks) {
