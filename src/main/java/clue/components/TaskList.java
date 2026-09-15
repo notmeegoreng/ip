c65eb8f8cd06eab262447d0c3db94f2192780fd0
notmeegoreng <notmeegoreng@gmail.com>
2026-09-16 06:58:34 +0800

    Update TaskList for better code quality
    There was minor duplicate code which might cause
    diverging UX in the future.
    
    Extracts out the common code into a private static function.
    Minor documentation edits.

--- src/main/java/clue/components/TaskList.java
+++ src/main/java/clue/components/TaskList.java
@@ -1,8 +1,6 @@
 package clue.components;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.TreeMap;
 
@@ -29,6 +27,7 @@ public class TaskList extends ArrayList<Task> {
     public void list(Ui ui) {
         if (isEmpty()) {
             ui.println("nothing here...");
+            return;
         }
 
         for (int i = 1; i <= size(); i++) {
@@ -121,7 +120,7 @@ public class TaskList extends ArrayList<Task> {
     }
 
     /**
-     * Reports the count of items list to the given Ui object.
+     * Reports the count of items in the list.
      */
     public void reportCount(Ui ui) {
         ui.printf(
@@ -137,7 +136,7 @@ public class TaskList extends ArrayList<Task> {
      * @param index the index of this task.
      * @param task the task to output.
      */
-    void printTask(Ui ui, int index, Task task) {
+    static void printTask(Ui ui, int index, Task task) {
         ui.printf(" %d. %s\n", index, task);
     }
 }
